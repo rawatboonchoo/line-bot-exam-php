c2c0df33c85a5465f89d1e6e4a6e75f598c9ab6c
togetter <boonchoo.rawat@gmail.com>
2018-05-01 22:06:47 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -1,20 +1,12 @@
 <?php
-
-
-
 require "vendor/autoload.php";
-
 $access_token = 'Dor2LyTSroakqqqtrx7G+N68hZiU7bFDkTL5tL1Z6Io/YfbJs3ZRjyupRlgtnSLLWy2USRiFyCrr98mdLA454RlTaTjsH5IX4YyGuBMbn8yJgXPiex0Bl+CipYIdYD1frhDokYJSdPapSFvsCasHeAdB04t89/1O/w1cDnyilFU=';
-
 $channelSecret = 'ccc3fb3084d5328cc3bb7470990552df';
-
-$idPush = 'U47fc805526e21297be23ff72cb6539fe';
-
+$idPush = 'U47fc805526e21297be23ff72cb6539fe'
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
-$response = $bot->pushMessage($pushID, $textMessageBuilder);
+$response = $bot->pushMessage('U7ef7a449f2a5c2057eacfc02ba2eb286', $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
