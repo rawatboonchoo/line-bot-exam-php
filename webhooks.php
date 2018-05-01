c1a57edf3f3cc2ba0793bf7ba84847165c7df0ec
togetter <boonchoo.rawat@gmail.com>
2018-05-01 22:49:48 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -16,7 +16,8 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['source']['userId'];
+			//$text = $event['source']['userId'];
+			$text = 'Hello bot line';
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
@@ -29,8 +30,7 @@ if (!is_null($events['events'])) {
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
-				//'replyToken' => $replyToken,
-				'replyToken' => 'TEST BOTLINE',
+				'replyToken' => $replyToken,
 				'messages' => [$messages],
 			];
 			$post = json_encode($data);
