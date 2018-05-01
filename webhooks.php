590da4c9ecd3504209d23cf7c0f9f11fed479f50
togetter <boonchoo.rawat@gmail.com>
2018-05-01 21:46:43 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'HhDeVpY6GNy05NepDh1SIeQTpgXd9m0TgPRIBgeHhoWswVf4z+ElPq7To1kXAShjWy2USRiFyCrr98mdLA454RlTaTjsH5IX4YyGuBMbn8yWQYpplGJ0K/CpdPnjIZ4E+SLI6mBUvEzErOKtfbEnrAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
