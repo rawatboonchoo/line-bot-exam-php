3a9c0a4227fc93ddd8e4035a4ecd73530ac79911
togetter <boonchoo.rawat@gmail.com>
2018-05-02 22:28:43 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -23,14 +23,13 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			   $messages = [
-					    'type' => 'text',
-					    'text' => $text
-					],[
-				    	    'type' => 'text',
-					    'text' => $text_txt
-			   		];
-
-
+					    //'type' => 'text',
+					    //'text' => $text
+				   	{'type':'text','text':$text},
+				   	{'type':'text','text':$text_txt}
+				   		
+					];
+			
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
