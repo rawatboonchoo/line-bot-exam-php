e7edb6f751cba36be713df04ff6d955befb3ac2d
togetter <boonchoo.rawat@gmail.com>
2018-05-02 22:34:44 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -23,24 +23,21 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			   $messages = [
-					    //'type' => 'text',
-					    //'text' => $text
-				   	{
-					    "type":"text",
-					    "text":"Hello, user"
-					},
-					{
-					    "type":"text",
-					    "text":"May I help you?"
-					}
-				   		
+					    'type' => 'text',
+					    'text' => $text
+	
+					];
+			 $messages_txt = [
+					    'type' => 'text',
+					    'text' => $text_txt
+	
 					];
 			
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
+				'messages' => [$messages,$messages_txt],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
