bb949c6d99e529fde4f86409fec39698e2937a82
togetter <boonchoo.rawat@gmail.com>
2018-05-02 21:48:36 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -24,8 +24,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text,
-				'text' => $text_txt
+				'text' => $text + $text_txt
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
