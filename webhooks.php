7c39b1916a9a63f1d7023871521007d3554d6772
togetter <boonchoo.rawat@gmail.com>
2018-05-02 22:15:54 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -24,10 +24,16 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				
-				'type' => 'text',
-				'text' => $text
+				//'type' => 'text',
+				//'text' => $text
+				"message": {
+				    "type": "text",
+				    "text": "Hello, world!"
+				  }
 				
 			];
+			
+
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
