53a3330692690a35c2b4aefa4d3f08fc930f5df9
togetter <boonchoo.rawat@gmail.com>
2018-06-08 09:25:47 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,9 +4,10 @@ $access_token = 'Dor2LyTSroakqqqtrx7G+N68hZiU7bFDkTL5tL1Z6Io/YfbJs3ZRjyupRlgtnSL
 $channelSecret = 'ccc3fb3084d5328cc3bb7470990552df';
 $idPush = 'U47fc805526e21297be23ff72cb6539fe';
 $url = 'https://www.lotto.ktbnetbank.com/';
+$msg = $_GET['msg'];
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($url);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($msg);
 $response = $bot->pushMessage($idPush, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
