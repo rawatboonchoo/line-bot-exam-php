00e36d7d5b50c912dd14824402635bfab9f8cdce
togetter <boonchoo.rawat@gmail.com>
2019-02-20 19:35:29 +0700

    Update spammer.php

--- spammer.php
+++ spammer.php
@@ -1,66 +1,26 @@
-</head>
+<!DOCTYPE html>
+<html>
 <body>
 
-<div id="response">
-    <pre></pre>
-</div>
+<h1>The XMLHttpRequest Object</h1>
 
-<form id="my-form">
-<p>
-<select name="method" id="method">
-<option value="SMS">SMS</option>
-<option value="CALL">CALL</option>
-</select>
-</p>
-<p>
-<select name="countryCode" id="countryCode">
-<option value="MY">MY</option>
-<option value="SG">SG</option>
-<option value="ID">ID</option>
-<option value="TH">TH</option>
-<option value="VN">VN</option>
-<option value="KH">KM</option>
-<option value="PH">PH</option>
-<option value="MM">MM</option>
-</select>
-</p>
+<button type="button" onclick="loadDoc()">Request data</button>
 
-<p>
-<input type="text" id="phoneNumber" name="phoneNumber" placeholder="Phone +66" value="66945499432" />
-</p>
-<p>
-<input type="text" id="templateID" name="templateID" value="" />
-</p>
-<p>
-<input type="text" id="numDigits" name="numDigits" value="5" />
-</p>
-    <button type="submit">Submit</button>
-</form>
-
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<p id="demo"></p>
+ 
 <script>
-    (function($){
-        function processForm( e ){
-            console.log('click');
-            $.ajax({
-                url: 'https://api.grab.com/grabid/v1/phone/otp',
-                dataType: 'text',
-                type: 'post',
-                contentType: 'application/x-www-form-urlencoded',
-                data: $(this).serialize(),
-                success: function( data, textStatus, jQxhr ){
-                    $('#response pre').html( data );
-                },
-                error: function( jqXhr, textStatus, errorThrown ){
-                    console.log( errorThrown );
-                }
-            });
-
-            e.preventDefault();
-        }
-
-        $('#my-form').submit( processForm );
-    })(jQuery);
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("POST", "https://api.grab.com/grabid/v1/phone/otp", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send("method=SMS&countryCode=TH&phoneNumber=66945499432&templateID=&numDigits=4");
+}
 </script>
+
 </body>
 </html>
