defdee8bfd7528132aaec87e6dce00ae3f7a3f67
togetter <boonchoo.rawat@gmail.com>
2019-02-20 19:40:05 +0700

    Update spammer.php

--- spammer.php
+++ spammer.php
@@ -1,26 +1,30 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>The XMLHttpRequest Object</h1>
-
-<button type="button" onclick="loadDoc()">Request data</button>
-
-<p id="demo"></p>
+<?php
+//API Url
+$url = 'https=>//api.grab.com/grabid/v1/phone/otp';
  
-<script>
-function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("POST", "https://api.grab.com/grabid/v1/phone/otp", true);
-  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xhttp.send("method=SMS&countryCode=TH&phoneNumber=66945499432&templateID=&numDigits=4");
-}
-</script>
-
-</body>
-</html>
+//Initiate cURL.
+$ch = curl_init($url);
+ 
+//The JSON data.
+$jsonData = array(
+    'method'=> 'SMS', 
+    'countryCode'=> 'TH', 
+    'phoneNumber'=>'66945499432', 
+    'templateID'=>'', 
+    'numDigits'=>8
+);
+ 
+//Encode the array into JSON.
+$jsonDataEncoded = json_encode($jsonData);
+ 
+//Tell cURL that we want to send a POST request.
+curl_setopt($ch, CURLOPT_POST, 1);
+ 
+//Attach our encoded JSON string to the POST fields.
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+ 
+//Set the content type to application/json
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type=> application/json')); 
+ 
+//Execute the request
+$result = curl_exec($ch);
