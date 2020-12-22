3e6c6a5861eab9bbdfe252b4b404f9ef67050525
togetter <boonchoo.rawat@gmail.com>
2020-12-22 13:31:02 +0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyAOs3TsCTLwGZJ-3KOuXFQ_Wp0g9Q68DxM",
+    authDomain: "natha-e4e83.firebaseapp.com",
+    databaseURL: "https://natha-e4e83.firebaseio.com",
+    projectId: "natha-e4e83",
+    storageBucket: "natha-e4e83.appspot.com",
+    messagingSenderId: "403103664489"
+    };
+    firebase.initializeApp(config);
